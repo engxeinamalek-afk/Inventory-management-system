7902bdb0aad69f50674a2a32feab15d5af4c6eba
Zeina <engxeinamalek@gmail.com>
2026-09-18 19:31:16 +0300

    Add Transaction Controller

--- config/container-config.php
+++ config/container-config.php
@@ -4,9 +4,10 @@ use App\core\Database;
 use App\repository\ProductRepository;
 use App\repository\InventoryRepository;
 use App\repository\SupplierRepository;
+use App\repository\TransactionRepository;
 use App\services\ProductService;
 use App\services\SupplierPriceService;
-
+use App\services\TransactionService;
 $container = new Container();
 
 $container->set('PDO', function($c) {
@@ -23,9 +24,18 @@ $container->set(InventoryRepository::class, function($c) {
 $container->set(SupplierRepository::class, function($c) {
     return new SupplierRepository($c->get('PDO')); 
 });
+$container->set(TransactionRepository::class, function($c) {
+    return new TransactionRepository($c->get('PDO')); 
+});
 $container->set(ProductService::class, function($c){
     return new ProductService( $c->get(ProductRepository::class) ,$c->get(InventoryRepository::class) );
 });
 $container->set(SupplierPriceService::class, function($c){
     return new SupplierPriceService( $c->get(ProductRepository::class) ,$c->get(SupplierRepository::class) );
+});
+$container->set(TransactionService::class, function($c){
+    return new TransactionService( $c->get(ProductRepository::class) ,
+                                    $c->get(SupplierRepository::class),
+                                    $c->get(TransactionRepository::class),
+                                    $c->get(InventoryRepository::class) );
 });
--- src/controllers/TransactionController.php
+++ src/controllers/TransactionController.php
@@ -1,6 +1,30 @@
 <?php
 namespace App\controllers;
 
+use App\repository\TransactionRepository;
+use App\entities\Transaction;
+use App\repository\ProductRepository;
+use App\services\TransactionService;
+
 class TransactionController{
+    private TransactionRepository $repo;
+    private ProductRepository $productRepo;
+    private TransactionService $service;
+    public function __construct($container)
+    {
+        $this->repo= $container->get(TransactionRepository::class);
+        $this->productRepo= $container->get(ProductRepository::class);
+        $this->service= $container->get(TransactionService::class);
+    }
     //عمليات البيع والشراء
+    public function store(array $request, $id){
+        // اضافة سجل وتحديث الكمية => سيرفس وترانزاكشن
+        $price= $this->productRepo->getPrice($id);
+        $tarnsaction= new Transaction($id,
+                                    $request['type'],
+                                    $request['quantity'],
+                                    $price,
+                                    $request['supplier_id']);
+        
+    }
 }
