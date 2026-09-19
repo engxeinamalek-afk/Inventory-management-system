9d5e0696c298df755b2b90a45117dbe4d537d996
Zeina <engxeinamalek@gmail.com>
2026-09-19 16:11:22 +0300

    Fix setPrice service & add exception to supplier controller

--- src/controllers/SupplierController.php
+++ src/controllers/SupplierController.php
@@ -4,6 +4,7 @@ namespace App\controllers;
 use App\repository\SupplierRepository;
 use App\entities\Supplier;
 use App\services\SupplierPriceService;
+use Exception;
 
 class SupplierController{
     private SupplierRepository $repo;
@@ -16,11 +17,49 @@ class SupplierController{
     //اضافة مصدر
     public function store($request){
         $supplier= new Supplier($request['name'] , $request['phone']);
-        $this->repo->create($supplier);
+        try{
+            $this->repo->create($supplier);
+            return [
+                "status" => 201,
+                "success" => true,
+                "message" => "Supplier created successfully!"
+            ];
+        }catch (\PDOException $e) {
+            return [
+                "status"  => 500,
+                "success" => false,
+                "message" => "A database error occurred."
+            ];
+        }catch(Exception $e){
+            return [
+                "status" => 500,
+                "success" => false,
+                "message" => $e->getMessage()
+            ];
+        }
     }
     // تحديد سعر منتج من مصدر
     public function setPrice(array $request ,$productId , $supplierId){
         // التحقق من وجود المنتج والمصدر => سيرفس
-        $this->service->setPrice($productId, $supplierId , $request['price']);
+        try{
+            $this->service->setPrice($productId, $supplierId , $request['price']);
+            return [
+                "status" => 201,
+                "success" => true,
+                "message" => "Price saved successfully!"
+            ];
+        }catch (\PDOException $e) {
+            return [
+                "status"  => 500,
+                "success" => false,
+                "message" => "A database error occurred."
+            ];
+        }catch(Exception $e){
+            return [
+                "status" => 500,
+                "success" => false,
+                "message" => $e->getMessage()
+            ];            
+        }
     }
 }
--- src/services/SupplierPriceService.php
+++ src/services/SupplierPriceService.php
@@ -3,13 +3,15 @@ namespace App\services;
 
 use App\repository\ProductRepository;
 use App\repository\SupplierRepository;
+use PDO;
 
 class SupplierPriceService{
     public function __construct(private ProductRepository $productRepo,
                                 private SupplierRepository $supplierRepo)
     {}
-    public function setPrice($productId , $supplierId , $price){
-        if( $this->productRepo->find($productId) && $this->supplierRepo->find($supplierId))
-            $this->supplierRepo->add($productId, $supplierId, $price);
+    public function setPrice($productId, $supplierId, $price) {
+        $this->productRepo->find($productId);
+        $this->supplierRepo->find($supplierId);
+        $this->supplierRepo->add($productId, $supplierId, $price);
     }
 }
