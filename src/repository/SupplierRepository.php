42999ad66a0e3cbd9e88bc344e192193cdb15e39
Zeina <engxeinamalek@gmail.com>
2026-09-20 11:56:45 +0300

    fix get supplier Price function

--- src/repository/SupplierRepository.php
+++ src/repository/SupplierRepository.php
@@ -74,6 +74,6 @@ class SupplierRepository{
         
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         
-        return (float) $row['price'];
+        return (float) $row['supplier_price'];
     }
 }
