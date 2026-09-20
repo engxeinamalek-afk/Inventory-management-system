d7160805c94f765ae13f5dfbdaa86c5aec5c7a48
Zeina <engxeinamalek@gmail.com>
2026-09-20 15:56:36 +0300

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 use App\controllers\ProductController;
+use App\controllers\SupplierController;
+use App\controllers\TransactionController;
+
 return [
     'POST' => [
         '/add' => ProductController::class.'@store',
-        '/update/{id}' => ProductController::class.'@update'
+        '/update/{id}' => ProductController::class.'@update',
+        '/addsupplier' => SupplierController::class.'@store',
+        '/setPrice/{productId}/{supplierId}' => SupplierController::class.'@setPrice',
+        '/addTransaction/{id}' => TransactionController::class.'@store'
+    ],
+    'GET' => [
+        '/getPurchase' => TransactionController::class.'@getPurchases',
+        '/getSales' => TransactionController::class.'@getSales'
     ]
 ];
